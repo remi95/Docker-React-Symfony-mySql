d3249fb9e32aa04cb24a1d6e59b213f325847abe
Rémi Mafat <remimafat@gmail.com>
2021-04-23 10:47:43 +0200

    Add custom logs

--- docker/back/entrypoint.sh
+++ docker/back/entrypoint.sh
@@ -1,14 +1,30 @@
 #!/bin/bash
 
-# Symfony project already created
+# =========
+# Utilities
+# =========
+
+lightGreen="\033[1;32m";
+violinIcon="\xf0\x9f\x8e\xbb"
+
+function log () {
+    echo -e "${violinIcon} ${lightGreen}$1";
+}
+
+# =================
+# Commands executed
+# =================
+
 if [ -f "composer.json" ]; then
+    log "Running composer install...";
     composer install;
+    
+    log "Updating database with migrations...";
     bin/console doctrine:migrations:migrate -n --allow-no-migration;
     bin/console cache:clear;
 else
-    # Init project
+    log "Initializing new Symfony project...";
     symfony new . --full --no-git;
-    sed -i "s|#!/usr/bin/env php|#!/usr/bin/env php8|i" bin/console;
     cp .env .env.local;
     sed -i '/^#/d' .env.local; # Remove all comments
     sed -i "s|DATABASE_URL=.*|DATABASE_URL=\"mysql://$MYSQL_USER:$MYSQL_PASSWORD@docker_mysql:3306/$MYSQL_DATABASE?serverVersion=5.7\"|i" .env.local;
@@ -18,4 +34,5 @@ fi
 
 chown -R www-data:www-data .;
 
+log "Symfony project ready ! Let's coding !";
 /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf;
--- docker/front/entrypoint.sh
+++ docker/front/entrypoint.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
 
-# React project already created
+# =========
+# Utilities
+# =========
+
+lightBlue="\033[1;34m";
+atomIcon="\xe2\x9a\x9b\xef\xb8\x8f";
+
+function log () {
+    echo -e "${atomIcon}  ${lightBlue}$1";
+}
+
+# =================
+# Commands executed
+# =================
+
 if [ -f "package.json" ]; then
+    log "Running npm install...";
     npm install;
 else
-    # Init project
+    rm ./*; # React need an empty directory to init project.
+    
+    log "Initializing new React project...";
     if [ $USE_TYPESCRIPT = 1 ]; then
         npx create-react-app . --template typescript;
     else
@@ -14,4 +31,5 @@ fi
 
 chown -R node:node .;
 
+log "Starting your React project... Let's coding !";
 su - node -c "cd $WORKDIR_FRONT && npm run start";
